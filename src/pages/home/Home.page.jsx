7674a47c75d48e6fa71dcfca3ef8c6da6602cf0c
morriswa <william@morriswa.org>
2025-01-28 10:00:44 -0600

    # start developing application color scheme

--- src/pages/home/Home.page.jsx
+++ src/pages/home/Home.page.jsx
@@ -4,6 +4,13 @@ import './Home.page.scss';
 function Home() {
   return (<>
     <h1>Hello React World!</h1>
+    <div className={"p-2 primary-background-color alternate-text-color "}>Primary Color</div>
+    <div className={"p-2 secondary-background-color "}>Secondary Color</div>
+    <div className={"p-2 tertiary-background-color "}>Tertiary Color</div>
+    <div className={"p-2 surface-background-color "}>Surface Color</div>
+    <div className={"p-2 alternate-surface-background-color "}>Alternate Surface Color</div>
+    <div className={"p-2 neutral-background-color "}>Neutral Color</div>
+    <div className={"p-2 alternate-neutral-background-color alternate-text-color "}>Alternate Neutral Color</div>
   </>)
 }
 
