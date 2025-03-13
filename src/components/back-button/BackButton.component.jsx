166c92820255d7e7f11e7743ad87c693352fd2d0
Kevin <kevin.rivers14832@ku.edu>
2025-03-12 20:41:25 -0500

    Documenting some of the code on the front end.

--- src/components/back-button/BackButton.component.jsx
+++ src/components/back-button/BackButton.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
 
+//This is a custom react component that is used to navigate back to the previous page. KR
+//It uses the useNavigate hook from react-router-dom to navigate back to the previous page. KR
 function BackButton() {
 
   const nav = useNavigate();
--- src/components/page-loader/PageLoader.component.jsx
+++ src/components/page-loader/PageLoader.component.jsx
@@ -1,4 +1,4 @@
-
+// This is a custom react component that displays the text "Thinking..." while the page is loading. KR
 function PageLoader() {
   return (
     <div className={"flex-col hfill wfill justify-content-center align-items-center app-background-color"}>
