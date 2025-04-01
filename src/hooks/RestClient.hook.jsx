f85a5f4350d12cbce4279c1bb5006886f6c2d24d
morriswa <william@morriswa.org>
2025-04-01 12:11:29 -0500

    # upgrade outdated deps # restore RestClient.hook.jsx, no changes were required

--- src/hooks/RestClient.hook.jsx
+++ src/hooks/RestClient.hook.jsx
@@ -2,49 +2,40 @@ import {useAuth0} from "@auth0/auth0-react";
 
 
 const jsonHttp = async (url, method, body = null, options = {}) => {
+
   let response = null;
   let METHOD = method.toUpperCase().trim();
- 
   if (METHOD === 'GET') {
     response = await fetch(url, {
       method: METHOD,
       headers: options.headers,
       contentType: "application/json",
-    });
+    })
   } else {
     response = await fetch(url, {
       method: METHOD,
       body: JSON.stringify(body),
       headers: options.headers,
       contentType: "application/json",
-    });
+    })
   }
 
-
-  // Check if response is OK
-  if (!response.ok) {
-    const error = await response.text();
-    throw new Error(`HTTP Error ${response.status}: ${error}`);
-  }
-
-
-  // Parse the JSON response
-  return response.json();
+  return response
 };
 
 
 class RestClient {
+
   _getAccessToken = ()=>null;
-  _baseUrl = import.meta.env.VITE_APP_API;
+  _baseUrl = import.meta.env.VITE_APP_API
 
 
   constructor(getAccessTokenFn) {
-    this._getAccessToken = getAccessTokenFn;
+    this._getAccessToken = getAccessTokenFn
   }
 
-
   get accessToken() {
-    return this._getAccessToken();
+    return this._getAccessToken()
   }
 
 
@@ -53,47 +44,49 @@ class RestClient {
       headers: {
         Authorization: `Bearer ${await this.accessToken}`,
       }
-    });
+    })
   }
-
 
   async health() {
-    return jsonHttp(`${this._baseUrl}/health`, 'GET');
+    return jsonHttp(`${this._baseUrl}/health`, 'GET')
   }
-
 
   async getUserPromptHistory() {
     return jsonHttp(`${this._baseUrl}/s/history`, 'GET', null, {
       headers: {
         Authorization: `Bearer ${await this.accessToken}`,
       }
-    });
+    })
   }
-
 
   async getChatLog(chatId) {
     return jsonHttp(`${this._baseUrl}/s/chat/${chatId}`, 'GET', null, {
       headers: {
         Authorization: `Bearer ${await this.accessToken}`,
       }
-    });
+    })
   }
 
-
   async createNewChat(promptText) {
-    return jsonHttp(`${this._baseUrl}/s/chat`, 'POST', {'promptText': promptText}, {
+    return jsonHttp(`${this._baseUrl}/s/chat`, 'POST', {'prompt_text': promptText}, {
       headers: {
         Authorization: `Bearer ${await this.accessToken}`,
       }
-    );
+    })
+  }
+
+  async followUpChat(chatId, promptText) {
+    return jsonHttp(`${this._baseUrl}/s/chat/${chatId}`, 'POST', {'prompt_text': promptText}, {
+      headers: {
+        Authorization: `Bearer ${await this.accessToken}`,
+      }
+    })
   }
 }
-
 
 const useRestClient = () => {
   const { getAccessTokenSilently } = useAuth0();
   return new RestClient(getAccessTokenSilently);
 };
-
 
 export default useRestClient;
